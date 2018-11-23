c45923c391b2becdffc90f5c1922c5822ea10595
lwtor <lwtor7@gmail.com>
2018-11-23 18:38:03 +0800

    add recursive

---
+++ 2_递归/step_demo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def step_calculate(num):
+    if num == 1:
+        return 1
+    if num == 2:
+        return 2
+    return step_calculate(num - 1) + step_calculate(num - 2)
+
+step_count = input("输入台阶数：")
+count = step_calculate(int(step_count))
+print('%s 个台阶，总共有 %d 种情况' %(step_count, count))
